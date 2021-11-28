863c8ffb075f7a456afc25cefe19659b21ab95c7
nkmurphy <nmurp2@pdx.edu>
2021-11-28 14:52:13 -0800

    Update eval.py
    Updated create_partitions to make permutations of the two target lists unioned together and return tuples of each permutation split in half

--- eval/eval.py
+++ eval/eval.py
@@ -1,4 +1,6 @@
 import numpy as np
+from itertools import permutations
+
 
 # target_words has pairs of sets X and Y which are words associated with concepts suspected to be biased
 # towards one gender or another, i.e. Career vs. Family or science vs. arts
@@ -13,6 +15,8 @@ def w2v_weat_eval(E, definitional_pairs, target_words):
         for fword in female:
             female_sum += np.cos(E.v(word), E.v(fword))
         return ((1 / (len(male))) * male_sum) - ((1 / (len(female))) * female_sum)
+
+    # takes definitional pairs and splits them into sets for each gender
     def build_gendered_lists(def_pairs):
         mwords = set()
         fwords = set()
@@ -20,6 +24,9 @@ def w2v_weat_eval(E, definitional_pairs, target_words):
             mwords.add(pair[0])
             fwords.add(pair[1])
         return mwords, fwords
+
+    # finds the test statistic, which we use to obtain the probability that
+    # the word embedding is biased for the given subject
     def find_test_statistic(pair, mwords, fwords):
         s1 = pair[0]  # subject one
         s2 = pair[1]  # subject two
@@ -30,17 +37,24 @@ def w2v_weat_eval(E, definitional_pairs, target_words):
         for word in s2:
             s2_sum += moa(word, mwords, fwords)
         return s1_sum, s2_sum
-    def create_partitions(union_set): # stub for now
-        pass
+
+    # creates permutations of the union set where
+    def create_partitions(union_set):
+        permutation = permutations(union_set)
+        part = list()
+        for perm in permutation:
+            part.append((perm[:len(perm) // 2], perm[len(perm) // 2:]))
+        return part
+
     mwords, fwords = build_gendered_lists(definitional_pairs)
     p_values = []
     for pair in target_words:
         test_statistic_greater = 0
-        o_sum = find_test_statistic(pair, mwords, fwords) # observed test statistic
+        o_sum = find_test_statistic(pair, mwords, fwords)  # observed test statistic
         partitions = create_partitions(pair[1].union(pair[0]))
         for partition in partitions:
             t_sum = find_test_statistic(partition, mwords, fwords)
             if (o_sum < t_sum):
                 test_statistic_greater += 1
-        p_values.append(test_statistic_greater/len(partitions))
+        p_values.append(test_statistic_greater / len(partitions))
     return p_values
